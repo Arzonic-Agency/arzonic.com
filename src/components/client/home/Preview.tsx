603dfb6afc00e3a1cd1bbc2a724c8689e1fc42e2
Marc Made It <spurdo.dk@gmail.com>
2025-04-29 13:23:36 +0200

    update

--- src/components/client/home/Preview.tsx
+++ src/components/client/home/Preview.tsx
@@ -1,19 +1,7 @@
 import React from "react";
 
 const Preview = () => {
-  return (
-    <div>
-      <div className="mockup-phone border-primary scale-50">
-        <div className="mockup-phone-camera"></div>
-        <div className="mockup-phone-display">
-          <img
-            alt="wallpaper"
-            src="https://img.daisyui.com/images/stock/453966.webp"
-          />
-        </div>
-      </div>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Preview;
