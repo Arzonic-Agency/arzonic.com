a181504b4a1c481ca5ef1caf323ff1e46ee78d8f
Xyrel Zukira <sebastian@birdie.codes>
2025-05-01 12:51:25 +0200

    Added Price Calculator

--- src/app/(client)/price-calculator/page.tsx
+++ src/app/(client)/price-calculator/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import PriceEstimator from '@/components/client/price-calculator/priceCalculator'
 
 const PriceCalculator = () => {
-  return <div>page</div>;
+  return (
+    <main className="min-h-screen bg-base-100 py-12">
+    <PriceEstimator />
+  </main>
+  )
 };
 
 export default PriceCalculator;
