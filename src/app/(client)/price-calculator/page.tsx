1a89a73c59197c9768ddc125957ef87732974ebd
Marc Made It <spurdo.dk@gmail.com>
2025-05-05 12:34:34 +0200

    Update page.tsx

--- src/app/(client)/price-calculator/page.tsx
+++ src/app/(client)/price-calculator/page.tsx
@@ -1,27 +1,27 @@
 // arzonic/src/app/(client)/price-calculator/page.tsx
-'use client';
+"use client";
 
-import React from 'react';
-import PriceEstimator from '@/components/client/price-calculator/PriceEstimator';
+import React from "react";
+import PriceEstimator from "@/components/client/price-calculator/PriceEstimator";
 import { useRive } from "@rive-app/react-canvas";
 
 export default function PriceCalculatorPage() {
   const { RiveComponent } = useRive({
-    src: '/rive/design.riv',
+    src: "/rive/design.riv",
     autoplay: true,
   });
 
   return (
-    <main className="min-h-screen flex items-center bg-base-100">
+    <div className="p-20 md:p-0 md:min-h-screen flex items-center justify-center bg-base-100 flex-col md:flex-row">
       {/* LEFT: sliding form */}
-      <div className="w-1/2 p-8">
+      <div className="h-full md:w-1/2 p-8 ">
         <PriceEstimator />
       </div>
 
       {/* RIGHT: Rive animation */}
-      <div className="w-1/2 p-8 flex justify-center items-center">
+      <div className="hidden md:flex md:w-1/2 p-8 justify-center items-center">
         <RiveComponent className="w-full max-w-3xl h-[500px]" />
       </div>
-    </main>
+    </div>
   );
 }
