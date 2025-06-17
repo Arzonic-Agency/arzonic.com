4162b2f7e176bc677c3d32334d7cf98498468884
Marc Made It <spurdo.dk@gmail.com>
2025-06-17 18:13:13 +0200

    Update page.tsx

--- src/app/(client)/page.tsx
+++ src/app/(client)/page.tsx
@@ -14,7 +14,7 @@ export default function Page() {
     <>
       <p className="sr-only">
         We are a software agency building custom websites, web applications, and
-        immersive 3D experiences for businesses across Europe
+        immersive 3D experiences for businesses across Europe.
       </p>
       <HomePage />
     </>
