f4b110fb327a412a2e9bbfd3c80436a13fa4f55e
Marc Made It <spurdo.dk@gmail.com>
2025-06-26 19:29:22 +0200

    Update route.ts

--- src/app/api/content/[lang]/route.ts
+++ src/app/api/content/[lang]/route.ts
@@ -1,19 +1,19 @@
 import path from "path";
 import fs from "fs/promises";
 import { CONTENT_PATH } from "@/lib/env";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
-  _: NextRequest,
+  _request: NextRequest,
   { params }: { params: { lang: string } }
 ) {
-  const lang = params.lang || "da";
+  const lang = params.lang || "da"; // fallback hvis ikke sat
   const filePath = path.join(CONTENT_PATH, `${lang}.json`);
 
   try {
     const data = await fs.readFile(filePath, "utf-8");
 
-    return new Response(data, {
+    return new NextResponse(data, {
       status: 200,
       headers: {
         "Content-Type": "application/json",
@@ -22,7 +22,7 @@ export async function GET(
     });
   } catch (err) {
     console.error("Kunne ikke læse oversættelse:", err);
-    return new Response(
+    return new NextResponse(
       JSON.stringify({ error: "Translation file not found or invalid" }),
       { status: 500 }
     );
