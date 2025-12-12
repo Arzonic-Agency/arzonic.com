be14e8ecedbf1c23740ce1d7d9da2a14599a6617
Marc <spurdo.dk@gmail.com>
2025-12-12 13:27:00 +0100

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -61,7 +61,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning data-theme="arzonicdark">
       <head>
         <script
           type="application/ld+json"
